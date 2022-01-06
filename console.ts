4553ab6e15fdfef9b6198beeea2f9ee20ea653bc
Michael Bianco <iloveitaly@gmail.com>
2022-01-06 11:40:48 -0700

    Console and common utils
    Pulled utils from another lunchmoney project

---
+++ console.ts
@@ -0,0 +1,15 @@
+import repl from "repl";
+import dotenv from "dotenv";
+import { LunchMoney } from "lunch-money";
+
+dotenv.config();
+
+// TODO can we add helper methds for debugging in the future?
+// https://stackoverflow.com/questions/31173473/list-all-global-variables-in-node-js
+
+const local = repl.start("> ");
+const lunchMoney = new LunchMoney({
+  token: process.env.LUNCH_MONEY_API_KEY || "",
+});
+
+local.context.lunchMoney = lunchMoney;
---
+++ util.ts
@@ -0,0 +1,71 @@
+import papaparse from "papaparse";
+import fs from "fs";
+
+/*
+< {
+<   orderid: 'D01-7098770-3645007',
+<   items: 'Elf; ',
+<   categories: '',
+<   to: '0',
+<   date: '2021-12-17',
+<   total: '0',
+<   shipping: '0',
+<   shipping_refund: '0',
+<   gift: '0',
+<   tax: '3.99',
+<   refund: '0',
+<   payments: '2021-12-17: $0.00; '
+< }
+*/
+
+export interface AmazonTransaction {
+  orderid: string;
+  items: string;
+  categories: string;
+  to: string;
+  date: string;
+  total: string;
+  shipping: string;
+  shipping_refund: string;
+  gift: string;
+  tax: string;
+  refund: string;
+  payments: string;
+}
+
+export const readJSONFile = (path: string): any | null => {
+  if (fs.existsSync(path)) {
+    return JSON.parse(fs.readFileSync(path, "utf8"));
+  }
+
+  return null;
+};
+
+// TODO should type the resulting object here for better checking downstream
+export const readCSV = async (
+  filePath: string
+): Promise<AmazonTransaction[]> => {
+  const csvFile = fs.readFileSync(filePath);
+  const csvData = csvFile.toString();
+
+  return new Promise((resolve) => {
+    papaparse.parse(csvData, {
+      header: true,
+      skipEmptyLines: true,
+      // 'Original Description' => 'OriginalDescription'
+      transformHeader: (header: string) => header.replace(/\s/g, ""),
+      complete: (results) => {
+        resolve(results.data);
+      },
+    } as papaparse.ParseConfig<AmazonTransaction>);
+  });
+};
+
+export const writeCSV = (csvRows: any, filePath: string) => {
+  const csvContent = papaparse.unparse(csvRows);
+  fs.writeFileSync(filePath, csvContent);
+};
+
+export function prettyJSON(json: Object, returnString = false): string {
+  return JSON.stringify(json, null, 2);
+}
